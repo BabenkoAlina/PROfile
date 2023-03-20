d777fa2066b8450c45715e0284560e9f5c2dcce4
khrystynamk <khrystyna.mysak@gmail.com>
2023-03-20 15:43:53 +0200

    Figured out data storing and simplified code.

--- goals.py
+++ goals.py
@@ -1,40 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request
+from services.list import *
+from services.goal import *
 
 app = Flask(__name__)
+# TODO: get id dynamically
+USER_ID=1
 
 @app.get("/")
 def index():
     """
     Get main page from templates.
     """
-    return render_template("goals_home.html")
+    return render_template("goals_home.html", lists=get_lists_by_user_id(USER_ID))
 
-@app.route('/per_week')
-def goals_week():
+@app.route('/<name>')
+def goals(name):
     """
     Redirect to the page with specific goals (weekly).
     """
-    return render_template('goals_per_week.html')
+    # if folder exists
+    # if no - error page
+    # get goals for current folder
 
-@app.route('/per_month')
-def goals_month():
-    """
-    Redirect to the page with specific goals (monthly).
-    """
-    return render_template('goals_per_month.html')
-
-@app.route('/per_year')
-def goals_year():
-    """
-    Redirect to the page with specific goals (yearly).
-    """
-    return render_template('goals_per_year.html')
+    goals = [] #get_goals(user_id, name)
+    return render_template('goals_list.html', list_name = name, goals = goals)
 
 @app.post('/')
 def add_folder():
-    folder_name = request.form.get('folder')
-    if request.method == "POST":
-        return render_template("goals_home.html", content = folder_name)
-    
+    """
+    Create new folder (list) for goals and save it for the user.
+    """
+    list_name = request.form.get('list')
+    create_list_by_user_id(USER_ID, list_name)
+
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run()
