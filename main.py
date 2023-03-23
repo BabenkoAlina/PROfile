6d3a93dbe29a29e62a1f5a0159a23da4c6c91d72
niuta24 <syvkova.pn@ucu.edu.ua>
2023-03-23 17:09:56 +0300

    Adding connected sign up and goals

---
+++ main.py
@@ -0,0 +1,102 @@
+from flask import Flask, session, render_template, request, redirect
+import pyrebase
+from services.list import *
+from services.goal import *
+
+app = Flask(__name__)
+# TODO: get id dynamically
+USER_ID=1
+
+firebaseConfig = {
+    'apiKey': "AIzaSyAXoO8gg9C8_osWeI3YgUoOZ5Y3_QndNiI",
+    'authDomain': "profile-3403b.firebaseapp.com",
+    'projectId': "profile-3403b",
+    'storageBucket': "profile-3403b.appspot.com",
+    'messagingSenderId': "346310265739",
+    'appId': "1:346310265739:web:97398d6f17b6945147fd8e",
+    'databaseURL': ""
+  }
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+app.secret_key = 'secret'
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    print(session)
+    if "email" not in session:
+        return redirect('/login')
+    else:
+        return render_template('goals_home.html')
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if "email" in session:
+        return "Logged in as " + session['email']
+    if request.method == 'POST':
+        email = request.json["email"]
+        password = request.json["password"]
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['email'] = email
+            return "", 302
+        except:
+            return '{"error": "Failed to login"}'
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if ('email' in session):
+        return "Logged in as " + session['email']
+    if request.method == 'POST':
+        email = request.json["email"]
+        password = request.json["password"]
+
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            session['email'] = email
+            write_csv(user['localId'], email)
+            return "", 302
+        except Exception as e:
+            return '{"error": "Failed to login"}', 200  
+    return render_template('signup.html')
+
+def write_csv(localId, email):
+    with open('users.csv', 'a') as f:
+        f.write(localId + ',' + email +'\n')
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect('/')
+
+@app.get("/goals")
+def index():
+    """
+    Get main page from templates.
+    """
+    return render_template("goals_home.html", lists=get_lists_by_user_id(USER_ID))
+
+@app.route('/<name>')
+def goals(name):
+    """
+    Redirect to the page with specific goals (weekly).
+    """
+    # if folder exists
+    # if no - error page
+    # get goals for current folder
+
+    goals = [] #get_goals(user_id, name)
+    return render_template('goals_list.html', list_name = name, goals = goals)
+
+@app.post('/folder')
+def add_folder():
+    """
+    Create new folder (list) for goals and save it for the user.
+    """
+    list_name = request.form.get('list')
+    create_list_by_user_id(USER_ID, list_name)
+
+    return redirect('/goals')
+
+if __name__ == '__main__':
+    app.run(port=1111, debug=True)
