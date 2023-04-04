b9df7303920d90157e41791b5fb673591bd23942
Alina Babenko <117819807+cyberprince2201@users.noreply.github.com>
2023-04-04 18:10:15 +0300

    Commit (return to roots)

--- habits.py
+++ habits.py
@@ -1,5 +1,11 @@
-from flask import render_template, redirect, url_for
+from flask import Flask, render_template, request, session
+import os
+import csv
+from flask import render_template
 import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import habits_add as ha
 
 app = ha.Flask(__name__, template_folder="templates")
@@ -7,9 +13,11 @@ app.config['SECRET_KEY'] = 'secret'
 # userID = session["localID"]
 # userID = 123
 
-def show_habits():
-    habits = ha.read_habits()
-    return render_template('habits.html', habits=habits)
+
+# function to read the habit data from CSV file
+def read_csv():
+    ha.read_habits()
+
 
 # home page route to display the habits list
 @app.route('/', methods=['GET', 'POST'])
@@ -23,30 +31,21 @@ def index():
     habits = ha.read_habits()
     return render_template('habits.html', habits=habits, today_str=today_str, **context)
 
-@app.route('/add_habit', methods=['GET', 'POST'])
+
+@app.route('/add_habit', methods=['POST'])
 def add_habit():
     task_form = ha.TaskForm()
     habit_name = task_form.task.data
-    habits = ha.write_new_habit(habit_name)
-    ha.write_habits(habits)
-    habit_id = ha.write_new_habit(habit_name)
-    new_habit = {'User ID': 1, 'Habit ID': habit_id, 'Name': habit_name, 'Count': 0}
-    habits.append(new_habit)
+    ha.write_new_habit(habit_name)
 
-    return redirect(url_for('index'))
-
-
-# function to write the habit data to CSV file
 def write_csv():
     habits = ha.read_habits()
     ha.write_habits(habits)
 
 # update habit route to update the count of a habit
-
 @app.route('/update_habit', methods=['GET', 'POST'])
 def update_habit():
-    ha.update_habit()
-    return redirect(url_for('index'))
+    return ha.index()
 
 if __name__ == '__main__':
     app.run(debug=True)
