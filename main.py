7a457b1e89b37781116fdc0c80292caf66c3747a
Alina Babenko <117819807+cyberprince2201@users.noreply.github.com>
2023-04-07 12:50:51 +0300

    New version

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import datetime as datet
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
 app = Flask(__name__)
 
 firebaseConfig = {
